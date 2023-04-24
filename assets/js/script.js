26873a680bd9fbe87b98e470961a9390f95aa126
ssamoht <tgaabriel444@gmail.com>
2023-04-24 17:39:26 -0300

    add line

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,9 +6,13 @@ const btnTodo = document.getElementById("btn-todo")
 
 function mostrar() {
     const imgTodo = document.getElementById("todo-img")
-    imgTodo.style.display = "flex";
-    imgTodo.style.width = "150px"
+    imgTodo.style.display = "flex"
+    imgTodo.style.width = "300px"
     imgTodo.style.zIndex = "9"
-    
- 
+    document.getElementById("gallery").style.border = "none"
+    document.getElementById("text-in-gallery").style.display = "none"
 } 
+
+document.getElementById("todo-img").addEventListener('click', function() {
+    window.open("pages/to-do-list", "_blank")
+})
