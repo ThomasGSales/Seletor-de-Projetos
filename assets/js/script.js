0689d75ddfcabcc7a4047a3f163c0c1f151eb851
ssamoht <tgaabriel444@gmail.com>
2023-04-24 16:40:29 -0300

    first commit

---
+++ assets/js/script.js
@@ -0,0 +1,14 @@
+// 
+const btnTodo = document.getElementById("btn-todo")
+
+
+// Funções
+
+function mostrar() {
+    const imgTodo = document.getElementById("todo-img")
+    imgTodo.style.display = "flex";
+    imgTodo.style.width = "150px"
+    imgTodo.style.zIndex = "9"
+    
+ 
+} 
