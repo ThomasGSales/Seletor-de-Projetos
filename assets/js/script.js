00572b3c380a365b8ff0dd6f9a5dbb6bba5c76f3
ssamoht <tgaabriel444@gmail.com>
2023-04-25 16:15:36 -0300

    correção dos botões

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,18 @@
-// 
-const btnTodo = document.getElementById("btn-todo")
-
 
 // Funções
 
-function mostrar() {
+function mostrar1() {
     const imgTodo = document.getElementById("todo-img")
+    const imgTime = document.getElementById("time-img")
+    const imgCube = document.getElementById("cube-img")
+    const imgLogin = document.getElementById("login-img")
+    const imgSpotify = document.getElementById("spotify-img")
+
+    imgCube.style.display = "none"
+    imgLogin.style.display = "none"
+    imgSpotify.style.display = "none"
+    imgTime.style.display = "none"
+
     imgTodo.style.display = "flex"
     imgTodo.style.width = "300px"
     imgTodo.style.zIndex = "9"
@@ -13,6 +20,87 @@ function mostrar() {
     document.getElementById("text-in-gallery").style.display = "none"
 } 
 
+function mostrar2() {
+    const imgTodo = document.getElementById("todo-img")
+    const imgTime = document.getElementById("time-img")
+    const imgCube = document.getElementById("cube-img")
+    const imgLogin = document.getElementById("login-img")
+    const imgSpotify = document.getElementById("spotify-img")
+
+    imgCube.style.display = "none"
+    imgLogin.style.display = "none"
+    imgSpotify.style.display = "none"
+    imgTodo.style.display = "none"
+
+    imgTime.style.display = "flex"
+    imgTime.style.width = "300px"
+    imgTime.style.zIndex = "9"
+    document.getElementById("gallery").style.border = "none"
+    document.getElementById("text-in-gallery").style.display = "none"
+} 
+
+function mostrar3() {
+    const imgTodo = document.getElementById("todo-img")
+    const imgTime = document.getElementById("time-img")
+    const imgCube = document.getElementById("cube-img")
+    const imgLogin = document.getElementById("login-img")
+    const imgSpotify = document.getElementById("spotify-img")
+
+    imgCube.style.display = "none"
+    imgLogin.style.display = "none"
+    imgSpotify.style.display = "none"
+    imgTodo.style.display = "none"
+
+    imgTime.style.display = "flex"
+    imgTime.style.width = "300px"
+    imgTime.style.zIndex = "9"
+    document.getElementById("gallery").style.border = "none"
+    document.getElementById("text-in-gallery").style.display = "none"
+} 
+
+function mostrar4() {
+    const imgTodo = document.getElementById("todo-img")
+    const imgTime = document.getElementById("time-img")
+    const imgCube = document.getElementById("cube-img")
+    const imgLogin = document.getElementById("login-img")
+    const imgSpotify = document.getElementById("spotify-img")
+
+    imgCube.style.display = "none"
+    imgLogin.style.display = "none"
+    imgSpotify.style.display = "none"
+    imgTodo.style.display = "none"
+
+    imgTime.style.display = "flex"
+    imgTime.style.width = "300px"
+    imgTime.style.zIndex = "9"
+    document.getElementById("gallery").style.border = "none"
+    document.getElementById("text-in-gallery").style.display = "none"
+} 
+
+function mostrar5() {
+    const imgTodo = document.getElementById("todo-img")
+    const imgTime = document.getElementById("time-img")
+    const imgCube = document.getElementById("cube-img")
+    const imgLogin = document.getElementById("login-img")
+    const imgSpotify = document.getElementById("spotify-img")
+
+    imgCube.style.display = "none"
+    imgLogin.style.display = "none"
+    imgSpotify.style.display = "none"
+    imgTodo.style.display = "none"
+
+    imgTime.style.display = "flex"
+    imgTime.style.width = "300px"
+    imgTime.style.zIndex = "9"
+    document.getElementById("gallery").style.border = "none"
+    document.getElementById("text-in-gallery").style.display = "none"
+} 
+
+
+
+
 document.getElementById("todo-img").addEventListener('click', function() {
     window.open("pages/to-do-list", "_blank")
 })
+
+
